9ac0514efdf6b20c3bd41d566937f3e61704a901
luya <91703950+lmaqungo@users.noreply.github.com>
2025-06-20 14:35:22 +0200

    included new custom enumerable method definitions
    Array#my_each
    Enumerable#my_each_with_index
    Enumerable#my_select
    Enumerable#my_all?
    Enumerable#my_any?
    Enumerable#my_none?
    Enumerable#my_count
    Enumerable#my_map
    Enumerable#my_inject

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,62 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    counter = 0
+    self.my_each do |element|
+      yield(element, counter)
+      counter+=1
+    end
+  end
+
+  def my_select
+    new_arr = []
+    self.my_each do |element|
+      new_arr << element if yield(element)
+    end
+    return new_arr
+  end
+
+  def my_all?
+    bool_tracker = false
+    self.my_each do |elem|
+      bool_tracker = yield(elem)
+    end
+    return bool_tracker
+  end
+
+  def my_any?
+    any_tracker = false
+    self.my_each do |elem|
+      any_tracker = yield(elem)
+      break if any_tracker
+    end
+    return any_tracker
+  end
+
+  def my_none?(&block)
+    arr = self.my_select(&block)
+    return arr.size== 0
+  end
+
+  def my_count(&block)
+    if block_given?
+      return self.my_select(&block).size
+    else
+      return self.size
+    end
+  end
+
+  def my_map
+    arr = []
+    self.my_each do |elem|
+      arr << yield(elem)
+    end
+    return arr
+  end
+
+  def my_inject(initial_value, &block)
+    self.reduce(initial_value, &block) 
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +65,9 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for element in self
+      yield(element)
+    end
+  end
 end
